34d62ea8120d0b2c67d2e952073ab5bb9da2dc7b
WNeira05 <walterneira1986@gmail.com>
2024-06-19 10:18:53 -0300

    dependecia thymeleaf

--- src/main/java/es/pildoras/proyecto1/Proyecto1/Controller/MIRestController.java
+++ src/main/java/es/pildoras/proyecto1/Proyecto1/Controller/MIRestController.java
@@ -1,14 +1,15 @@
 package es.pildoras.proyecto1.Proyecto1.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class MIRestController {
 
     @GetMapping("/")
     public String saludo(){
-        return "Hola alumnos de Spring boot";
+        return "index";
     }
 
 
