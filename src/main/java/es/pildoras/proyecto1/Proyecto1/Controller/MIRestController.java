af2055f660552abf55e9154362c4b0cf3b7a7a26
WNeira05 <walterneira1986@gmail.com>
2024-09-24 17:49:56 -0300

    DevTools dependency

--- src/main/java/es/pildoras/proyecto1/Proyecto1/Controller/MIRestController.java
+++ src/main/java/es/pildoras/proyecto1/Proyecto1/Controller/MIRestController.java
@@ -2,14 +2,20 @@ package es.pildoras.proyecto1.Proyecto1.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class MIRestController {
 
     @GetMapping("/")
     public String saludo(){
         return "index";
+    }
+
+    @GetMapping("/info")
+    public String informacion(){
+        return "Esta es la web de los mejores programadores del mundo mundial";
     }
 
 
